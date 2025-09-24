8d37fc3b1e3b888e0815d7150fa3ca5604a29209
Patrick <patrick.jvc@gmail.com>
2025-09-24 19:40:51 +0100

    Alteracoes no teste automatizado

---
+++ cypress/e2e/pre-cadastro.cy.js
@@ -0,0 +1,27 @@
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade Pré cadastro', () => {
+beforeEach(() => {
+  cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+  
+});
+
+  it('Deve completar o pré cadastro com sucesso', () => {
+    let nomeFaker = faker.person.firstName()
+    let sobrenomeFaker = faker.person.lastName()
+    let emailFaker = faker.internet.email()
+
+    cy.get('#reg_email').type(emailFaker)
+    cy.get('#reg_password').type('123456')
+    cy.get('input[name="register"]').click()
+
+    cy.contains('a', 'Detalhes da conta').click()
+
+    cy.get('#account_first_name').type(nomeFaker)
+    cy.get('#account_last_name').type(sobrenomeFaker)
+    cy.contains('button', 'Save changes').click()
+    cy.contains('Detalhes da conta modificados com sucesso.').should('be.visible')
+    
+
+  })
+})
---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,36 @@
+describe('Funcionalidade Página de produtos', () => {
+  beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+  })
+
+  it('Deve selecionar um produto da lista ', () => {
+    cy.get('a[title="Abominable Hoodie"]')
+    cy.contains('a', 'Abominable Hoodie')
+      .click()
+  })
+
+  it.only('Deve adicionar um produto ao carrinho', () => {
+
+    var quantidade = 3 
+
+    cy.get('a[title="Ariel Roll Sleeve Sweatshirt"]')
+    cy.contains('a', 'Ariel Roll Sleeve Sweatshirt')
+      .click()
+    cy.contains('.button-variable-item', 'Red')
+    .click()
+    cy.get('.button-variable-item-Red')
+      .click()
+    cy.get('.button-variable-item-XS')
+      .click()
+
+    cy.get('[name="quantity"]').clear().type(quantidade)
+
+    cy.contains('button', 'Comprar').click()
+
+    cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+    cy.get('.woocommerce-message')
+    .should('contain', quantidade + ' × “Ariel Roll Sleeve Sweatshirt” foram adicionados no seu carrinho.')
+
+    
+  })
+})
